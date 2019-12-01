aa9b9e3b824979dd0a609b80dd2bde3847fcc8da
Andrey Ruzinskiy <andrei.ruzinskii@ya.ru>
2019-12-01 16:17:51 +0300

    Update files

---
+++ spisok.sql
@@ -0,0 +1,169 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Дек 01 2019 г., 16:16
+-- Версия сервера: 10.3.13-MariaDB-log
+-- Версия PHP: 7.1.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `spisok`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `login` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Дамп данных таблицы `admins`
+--
+
+INSERT INTO `admins` (`id`, `name`, `login`, `password`) VALUES
+(1, 'Андрей', 'iozia', '7915aec80fe8af33485cb344d25adac9');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `journal`
+--
+
+CREATE TABLE `journal` (
+  `id` int(10) UNSIGNED ZEROFILL NOT NULL,
+  `worker` int(7) UNSIGNED ZEROFILL NOT NULL DEFAULT 0000000,
+  `ardate` datetime DEFAULT NULL,
+  `oudate` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Дамп данных таблицы `journal`
+--
+
+INSERT INTO `journal` (`id`, `worker`, `ardate`, `oudate`) VALUES
+(0000000001, 0000001, '2019-09-12 08:06:36', '2019-09-12 17:49:51'),
+(0000000002, 0000003, '2019-09-12 08:15:11', '2019-09-12 19:01:15'),
+(0000000003, 0000002, '2019-09-12 10:21:33', '2019-09-12 15:45:36');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `position`
+--
+
+CREATE TABLE `position` (
+  `id` int(5) UNSIGNED ZEROFILL NOT NULL,
+  `name` char(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Дамп данных таблицы `position`
+--
+
+INSERT INTO `position` (`id`, `name`) VALUES
+(00001, 'Инженер'),
+(00002, 'Бухгалтер'),
+(00003, 'Директор'),
+(00004, 'Зам. диреткора по АХР');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `workers`
+--
+
+CREATE TABLE `workers` (
+  `id` int(7) UNSIGNED ZEROFILL NOT NULL,
+  `fio` char(70) COLLATE utf8_unicode_ci NOT NULL,
+  `position` int(5) UNSIGNED ZEROFILL NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Дамп данных таблицы `workers`
+--
+
+INSERT INTO `workers` (`id`, `fio`, `position`) VALUES
+(0000001, 'Иванов Сергей Петрович', 00003),
+(0000002, 'Захаров Алексей Викторович', 00001),
+(0000003, 'Макарина Инна Сергеевна', 00004),
+(0000004, 'Фролова Мария Игоревна', 00002);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `journal`
+--
+ALTER TABLE `journal`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `position`
+--
+ALTER TABLE `position`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `workers`
+--
+ALTER TABLE `workers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `journal`
+--
+ALTER TABLE `journal`
+  MODIFY `id` int(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT для таблицы `position`
+--
+ALTER TABLE `position`
+  MODIFY `id` int(5) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT для таблицы `workers`
+--
+ALTER TABLE `workers`
+  MODIFY `id` int(7) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
